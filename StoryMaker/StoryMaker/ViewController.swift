716c57d10a8b765c98d62d53693a99daca3f471b
busesahinbas <buse.sahinbas98@gmail.com>
2025-01-17 19:55:17 +0300

    Update ViewController

--- StoryMaker/StoryMaker/ViewController.swift
+++ StoryMaker/StoryMaker/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var selectImageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    private func setupUI() {
+        if traitCollection.userInterfaceStyle == .dark {
+            view.backgroundColor = UIColor.systemIndigo.withAlphaComponent(0.3)
+        } else {
+            view.backgroundColor = UIColor.systemIndigo.withAlphaComponent(0.1)
+        }
+        
+        welcomeLabel.text = "StoryMaker'a Hoşgeldiniz"
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        welcomeLabel.textColor = .label
+        
+        selectImageButton.setTitle("Fotoğraf Seç", for: .normal)
+        selectImageButton.backgroundColor = .systemIndigo
+        selectImageButton.setTitleColor(.white, for: .normal)
+        selectImageButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func selectImageTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            let editVC = EditViewController(nibName: "EditViewController", bundle: nil)
+            editVC.selectedImage = selectedImage
+            editVC.modalPresentationStyle = .fullScreen
+            picker.dismiss(animated: true) {
+                self.present(editVC, animated: true)
+            }
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            if traitCollection.userInterfaceStyle == .dark {
+                view.backgroundColor = UIColor.systemIndigo.withAlphaComponent(0.3)
+            } else {
+                view.backgroundColor = UIColor.systemIndigo.withAlphaComponent(0.1)
+            }
+        }
+    }
 }
 
